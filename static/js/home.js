174600685a5e6fac2a547832fe81521f6ccca80b
asiriamalk <asiri.15@cse.mrt.ac.lk>
2020-11-10 14:53:29 +0530

    fix ui changes

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,22 @@
 jarallax(document.querySelectorAll('.jarallax'), {
     speed: 0.2
 });
+$(window).scroll(function() {
+
+  var scrollTop = $(window).scrollTop();
+  var blurFactor = 25; // Decrease number for more intensity
+  var blurstring = "blur("+scrollTop/blurFactor+"px)";
+  $("#image").css({
+    "-webkit-filter": blurstring,
+    "filter": blurstring
+  });
+
+  var newCov = ('0.1' * scrollTop) / 80;
+  // Decrease last number above to fade faster
+  $('#cover').css('opacity', newCov);
+  $("#cover").css({
+    "-webkit-filter": blurstring,
+    "filter": blurstring
+  });
+
+});
